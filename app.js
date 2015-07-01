830f2747670d8d6b59de75e37c5f8bac6efb9ed9
ponsfrilus <ponsfrilus@gmail.com>
2015-07-01 15:59:07 +0200

    Passport-tequila implemented

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 var express = require('express');
 var path = require('path');
 
-var alertSource = require('./monitor-websites');
+
+var passport = require('passport');
+var TequilaStrategy = require('passport-tequila').Strategy;
+function myVerify(accessToken, refreshToken, profile, done) {
+    // Pretend the verification is asynchronous (as would be required
+    // e.g. if using a database):
+    process.nextTick(function () {
+        done(null, profile);
+    });
+}
+// Use the TequilaStrategy within Passport.
+var tequila = new TequilaStrategy({
+    service: "Is it up ?",
+    request: ["displayname"]
+    // require: "group=openstack-sti",  // Uncomment and use a group you are a member of.
+}, myVerify);
+passport.use(tequila);
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+var alertSource = require('./lib/monitor-websites');
+var alertBag = require('./lib/alert-bag')();
 
 var app = express();
+//app.set('view engine', 'html');
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-var alertBag = require('./alert-bag')();
 
 app.use(express.static(path.join(__dirname, "./app")));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
@@ -19,6 +45,21 @@ app.post('/api/acknowledgement', function(req, res) {
     res.json(alertBag.acknowledgeAlert(req.body.alertId));
 });
 
+// This is how you Tequila-protect a page:
+app.get('/private', tequila.ensureAuthenticated, function(req, res){
+    res.write("Private page");
+    res.render('index');
+    //res.render('private', { user: req.user });
+});
+
+// To log out, just drop the session cookie.
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+
+// Alternatively, we can also log out from Tequila altogether.
+app.get('/globallogout', tequila.globalLogout("/"));
 
 
 alertSource.run(alertBag);
