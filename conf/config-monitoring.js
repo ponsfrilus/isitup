884e0682f76a6fbaf9a8759869b0ad086e9ac5ee
Dominique Quatravaux <dominique@quatravaux.org>
2015-07-31 13:50:21 +0200

    Refactoring : alertes flexibles
    * Les tests d'alerte sont des fonctions
    * Elles définissent le message d'erreur qu'elles souhaitent (en réglant le champ .details de l'argument du callback)

--- conf/config-monitoring.js
+++ conf/config-monitoring.js
@@ -1,13 +1,16 @@
-var websites = {
-    /**
-     * "website": [{"url": "http://www.ebsi.te", "mode": "up"}],
-     *   Note:
-     *       - mode "up" send message if statusCode == 200
-     *       - mode "down" send message if statusCode != 200
-     **/
-    "epfl": [{"url": "http://www.epfl.ch", "mode": "up"}],
-    "google": [{"url": "http://www.google.ch", "mode": "down"}],
-    //"fake": [{"url": "http://www.thisisfake.ch", "mode": "down"}]
-};
 
-exports.websites = websites;
+
+exports.websites = function(checks){
+    return {
+        /**
+         * "website": [{"url": "http://www.ebsi.te", "mode": "up"}],
+         *   Note:
+         *       - mode "up" send message if statusCode == 200
+         *       - mode "down" send message if statusCode != 200
+         **/
+        "epfl": [{"url": "http://www.epfl.ch", check: checks.ensureUp}],
+        "google": [{"url": "http://www.google.ch", check: checks.ensureUp}],
+        "fake": [{"url": "http://www.thisisfake.ch", check: checks.ensureUp}]
+    };
+
+};
